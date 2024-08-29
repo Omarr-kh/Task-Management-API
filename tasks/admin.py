846eaf36876b38f02103988612fc4b8860050feb
Omar <omarkhaled1752003@yahoo.com>
2024-08-29 22:38:19 +0300

    register all models into the panel

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Task, Profile, ActivityLog
 
-# Register your models here.
+admin.site.register(Task)
+admin.site.register(Profile)
+admin.site.register(ActivityLog)
